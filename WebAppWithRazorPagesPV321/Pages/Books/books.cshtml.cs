1532d41ae2194ac457ce1584e85d32d2c4f4d60c
TSShrol <shrolts@gmail.com>
2024-08-21 19:35:47 +0300

    Add project files.

---
+++ WebAppWithRazorPagesPV321/Pages/Books/books.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppWithRazorPagesPV321.Pages.Books
+{
+    public class booksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
