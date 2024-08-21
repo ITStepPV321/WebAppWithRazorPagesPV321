4eb6b4ed475341c510034af5e71fe7794055b037
TSShrol <shrolts@gmail.com>
2024-08-21 20:42:37 +0300

    Added RazorPages and used syntasic RazorPages

--- WebAppWithRazorPagesPV321/Pages/Books/books.cshtml.cs
+++ WebAppWithRazorPagesPV321/Pages/Books/books.cshtml.cs
@@ -3,10 +3,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebAppWithRazorPagesPV321.Pages.Books
 {
-    public class booksModel : PageModel
+    public class BooksModel : PageModel
     {
+        public string UserName { get; set; }
+        public BooksModel()
+        {
+           UserName = "Admin";
+            
+        }
+
+        public string PrintShortDate() {
+            return $"Current Date: {DateTime.Now.Date.ToShortDateString()}";
+        }
         public void OnGet()
         {
+
         }
     }
 }
